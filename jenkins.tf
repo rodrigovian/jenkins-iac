5fa3e05899aef43c5fb516c501aa7285fe4029e5
terraform-server <terraform-server@ec2.internal>
2023-03-07 00:33:07 +0000

    Jenkins Server Config

--- jenkins.tf
+++ jenkins.tf
@@ -9,3 +9,31 @@ module "jenkins_sg" {
     ingress_rules       = ["http-80-tcp"]
     egress_rules        = ["all-all"]
 }
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "jenkins-server"
+
+  ami                    = "ami-006dcf34c09e50022"
+  instance_type          = "t3.micro"
+  key_name               = "vockey"
+  monitoring             = true
+  vpc_security_group_ids = [module.jenkins_sg.security_group_id]
+  subnet_id              = "subnet-00b89cde12ccb3a57"
+  user_data              = file("./jenkins-install.sh")
+  iam_instance_profile   = "LabInstanceProfile"
+
+
+  tags = {
+    Name = "Jenkins-Server"
+    Environment = "Prod"
+  }
+}
+
+resource "aws_eip" "jenkins-ip" {
+  instance = module.ec2_instance.id
+  vpc      = true
+}
+
